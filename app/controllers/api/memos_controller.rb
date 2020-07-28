af76b72a482e9d40a345f725cb11eef66b1062b7
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2020-07-28 18:37:55 +0900

    Update memos_controller.rb

--- app/controllers/api/memos_controller.rb
+++ app/controllers/api/memos_controller.rb
@@ -2,17 +2,11 @@ class Api::MemosController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create, :getTweets]
 
   def create
-    memo = Memmo.new(create_params)
-
-    unless memo.save
-      @error_message = [memo.errors.full_messages].compact
-    end
-  end
-
-  def getTweets
-    respond_to do |format|
-      format.html { redirect_to root_path }
-      format.json
+    if params != ""
+      memo = Memmo.new(create_params)
+      unless memo.save
+        @error_message = [memo.errors.full_messages].compact
+      end
     end
   end
 
