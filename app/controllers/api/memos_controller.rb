939a545da83a7a3a9469c539432160c351ade974
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2020-07-28 17:06:20 +0900

    getTweets追加

--- app/controllers/api/memos_controller.rb
+++ app/controllers/api/memos_controller.rb
@@ -1,5 +1,5 @@
 class Api::MemosController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: :create
+  skip_before_action :verify_authenticity_token, only: [:create, :getTweets]
 
   def create
     memo = Memmo.new(create_params)
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   $date = Time.now.in_time_zone('Tokyo').to_s
   root "api/memos#index"
   namespace :api, default: {format: :json} do
-    resources :memos, only: [:index, :create]
+    resources :memos, only: [:index, :create] do
+      collection do
+        get "getTweets"
+      end
+    end
   end
 end
