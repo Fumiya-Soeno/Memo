df68924171ec45e4cc0659c27f66959eb27fe567
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2020-07-28 12:03:58 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  $date = Time.now.to_s
+  $date = Time.now.in_time_zone('Tokyo').to_s
   root "api/memos#index"
   namespace :api, default: {format: :json} do
     resources :memos, only: [:index, :create]
