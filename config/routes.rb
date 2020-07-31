0e2789f0177aa94a025878a784ffb925f2167a29
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2020-07-31 17:49:43 +0900

    ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   $date = Time.now.in_time_zone('Tokyo').to_s
   root "api/memos#index"
   namespace :api, default: {format: :json} do
-    resources :memos, only: [:index, :create] do
-      collection do
-        post "getTweets"
-      end
-    end
+    resources :memos, only: [:index, :create]
   end
 end
